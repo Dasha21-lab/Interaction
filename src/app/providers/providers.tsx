10c453534bdb6135092eef5a1ab92e13bbb3e0d2
Дарья <chernyshov387@gmail.com>
2026-03-03 17:37:58 +0300

    main

---
+++ src/app/providers/providers.tsx
@@ -0,0 +1,16 @@
+import type { ReactNode } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux'
+import store from '../store/store';
+
+interface ProvidersProps {
+    children: ReactNode;
+}
+
+export const Providers = ({ children }: ProvidersProps) => {
+    return (
+        <BrowserRouter>
+           <Provider store={store}>{children}</Provider>
+        </BrowserRouter>
+    )
+};
---
+++ src/app/routers/AppRouter.tsx
@@ -0,0 +1,10 @@
+import { Route, Routes } from 'react-router-dom';
+
+export function AppRouter() {
+    return (
+        <Routes>
+            <Route path="/" element={<div>Home Page</div>} />
+        </Routes>
+    )
+}
+
