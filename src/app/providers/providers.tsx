5164312ab58b31c01941e4141b78706f05634e10
Дарья <chernyshov387@gmail.com>
2026-03-03 17:53:02 +0300

    main

--- src/app/providers/providers.tsx
+++ src/app/providers/providers.tsx
@@ -1,16 +1,16 @@
-import type { ReactNode } from 'react';
-import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux'
-import store from '../store/store';
+import type { ReactNode } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "../store/store";
 
 interface ProvidersProps {
-    children: ReactNode;
+  children: ReactNode;
 }
 
 export const Providers = ({ children }: ProvidersProps) => {
-    return (
-        <BrowserRouter>
-           <Provider store={store}>{children}</Provider>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <Provider store={store}>{children}</Provider>
+    </BrowserRouter>
+  );
 };
